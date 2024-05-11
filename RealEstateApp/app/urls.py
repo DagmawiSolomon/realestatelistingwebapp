1d6990c6fb04ff181aa0982c4eb7d86f47836cc9
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-05-11 20:17:18 +0300

    Display listings and agents on the frontend

--- RealEstateApp/app/urls.py
+++ RealEstateApp/app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.Home.as_view(), name="home")
+    path("", views.Home.as_view(), name="home"),
+    path("listing/",views.Listing.as_view(), name="listing"),
+    path("agent/", views.Agents.as_view(), name="agent",)
 ]
--- RealEstateApp/app/views.py
+++ RealEstateApp/app/views.py
@@ -1,9 +1,18 @@
 from typing import Any
 from django.shortcuts import render
-from .models import Listing
+from .models import Listing, Agent
 from django.views.generic import ListView
 
 
 class Home(ListView):
     model = Listing
     template_name = "app/home.html"
+
+
+class Listing(ListView):
+    model = Listing
+    template_name = "app/listing.html"
+
+class Agents(ListView):
+    model = Agent
+    template_name = "app/agent.html"
